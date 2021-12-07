34e200c383ad4b3b09d616bbb60a24e6d50350b8
riley <rileyarnholt@gmail.com>
2021-12-07 13:57:23 -0500

    Updated Files (1)

--- Mini_Project.py
+++ Mini_Project.py
@@ -11,37 +11,22 @@ highTempF = np.array(bike_data_total.iloc[:, 2].values)
 lowTempF = np.array(bike_data_total.iloc[:, 3].values)
 Precip = np.array(bike_data_total.iloc[:, 4].values)
 
-#Bridge data as Panda Series
-BrooklynS = bike_data_total.iloc[:, 5]
-ManhattanS = bike_data_total.iloc[:, 6].values
-WilliamsburgS = bike_data_total.iloc[:, 7].values
-QueensboroS = bike_data_total.iloc[:, 8].values
-allBridgeDataS = [Brooklyn, Manhattan, Williamsburg, Queensboro]
-TotalTravelersS = bike_data_total.iloc[:, 9].values
-
-#Bridge data as Numpy Arrays
+#Bridge data
+Brooklyn = np.array(pandas.array(bike_data_total.iloc[:, 5].values))
+Manhattan = np.array(pandas.array(bike_data_total.iloc[:, 6].values))
+Williamsburg = np.array(pandas.array(bike_data_total.iloc[:, 7].values))
+Queensboro = np.array(pandas.array(bike_data_total.iloc[:, 8].values))
+TotalTravelers = np.array(pandas.array(bike_data_total.iloc[:, 9].values))
+allBridgeData = [Brooklyn, Manhattan, Williamsburg, Queensboro]
 
 #converts bridge data into integers
 for bridge in allBridgeData:
+    tempVals = []
     for travelers in bridge:
-        travelers = int(travelers.replace(',', ''))
-        print(type(temp))
+        tempVals.append(int(travelers.replace(',', '')))
 
-print(type(BrooklynDF[0]))
 
-#1: which bridges should the sensors be installed on to get the best sense of total traffic
-#open to ideas here, the way it will be written below is by taking the average of each data set, finding how many
-#data points are higher than that and counting them, then choosing which three bridges have the highest count
 
-#way 1
-highTrafficBridges = []
-for bridge in allBridgeData:
-    highTrafficDays = 0
-    AvgTravelers = np.average(bridge)
-    for travelers in bridge:
-        if travelers > AvgTravelers:
-            highTrafficDays += 1
-    highTrafficBridges.append(highTrafficDays)
+print(type(Brooklyn[0]))
 
-#print(highTrafficBridges)
-
+#def trendFinder():
